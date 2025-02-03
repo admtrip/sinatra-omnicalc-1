3b0de35d97dceb9a2ba4ad7239efb2ad6194d21a
admtrip <atripat0@chicagobooth.edu>
2025-02-03 23:40:12 +0000

    midway

--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_form)
+end
+
+get("/square_root/results") do
+  @the_num_2 = params.fetch("users_number_2").to_f
+
+  @square_root = @the_num_2 ** 0.5
+  
+  erb(:square_form_results)
 end
